55764748065632c9015ad6ce764dcd3f0aed313c
BeniaminC <benjamin.condrea@gmail.com>
2024-03-09 17:57:01 -0800

    Wrote most of docstrings

--- mmr_systems/common/statistics.py
+++ mmr_systems/common/statistics.py
@@ -2,34 +2,140 @@ import numpy as np
 from scipy.stats import logistic, norm, rv_continuous, skewnorm
 
 
-def generate_normal_ratings_perc(low_perc, hi_perc, n, mu, sig):
+def generate_normal_ratings_perc(n: int,low_perc: float=0.01, hi_perc: float=0.99, mu: float=0., sig: float=1.):
+    '''
+    Generate a normal distribution based on percent range.
+
+    Args:
+        n (:obj:`int`): The number of data points.
+
+        low_perc (:obj:`float`): The lower bound percent.
+
+        hi_perc (:obj:`float`): The upper bound percent.
+    
+    Returns:
+        obj:`np.ndarray`: A normal distribution of the number of data points.
+    '''
     return norm.ppf(np.linspace(low_perc, hi_perc, n), mu, sig)
 
 
-def generate_logistic_ratings_perc(low_perc, hi_perc, n, mu, sig):
+def generate_logistic_ratings_perc(n: int,low_perc: float=0.01, hi_perc: float=0.99, mu: float=0., sig: float=1.):
+    '''
+    Generate a logistic distribution based on percent range.
+
+    Args:
+        n (:obj:`int`): The number of data points.
+
+        low_perc (:obj:`float`): The lower bound percent.
+
+        hi_perc (:obj:`float`): The upper bound percent.
+
+        mu (:obj:`float`): The expected value (mu) of the normal distribution.
+
+        sig (:obj:`float`): The deviation (sigma) of the normal distribution.
+
+    Returns:
+        obj:`np.ndarray`: A logistic distribution of the number of data points.
+    '''
     return logistic.ppf(np.linspace(low_perc, hi_perc, n), mu, sig)
 
 
-def generate_skewnormal_ratings_perc(low_perc, hi_perc, n, mu, sig, a):
+def generate_skewnormal_ratings_perc(n: int,low_perc: float=0.01, hi_perc: float=0.99, mu: float=0., sig: float=1., a: float=0.):
+    '''
+    Generate a logistic distribution based on percent range.
+
+    Args:
+        n (:obj:`int`): The number of data points.
+
+        low_perc (:obj:`float`): The lower bound percent.
+
+        hi_perc (:obj:`float`): The upper bound percent.
+
+        mu (:obj:`float`): The expected value (mu) of the normal distribution.
+
+        sig (:obj:`float`): The deviation (sigma) of the normal distribution.
+
+        a (:obj:`float`): Skewness parameter. When a = 0, it is identical to a normal distribution.
+
+    Returns:
+        obj:`np.ndarray`: A logistic distribution of the number of data points.
+    '''
     return skewnorm.ppf(np.linspace(low_perc, hi_perc, n), loc=mu, scale=sig, a=a)
 
 
-def generate_normal_ratings(low_rating, hi_rating, n, mu, sig):
+def generate_normal_ratings(n: int, low_rating: float=-3, hi_rating: float=3., mu: float=0., sig: float=1.):
+    '''
+    Generate a logistic distribution based on percent range.
+
+    Args:
+        n (:obj:`int`): The number of data points.
+
+        low_rating (:obj:`float`): The lower bound rating.
+
+        hi_rating (:obj:`float`): The upper bound rating.
+
+        mu (:obj:`float`): The expected value (mu) of the normal distribution.
+
+        sig (:obj:`float`): The deviation (sigma) of the normal distribution.
+
+        a (:obj:`float`): Skewness parameter. When a = 0, it is identical to a normal distribution.
+
+    Returns:
+        obj:`np.ndarray`: A logistic distribution of the number of data points.
+    '''
     norm_dist = norm(mu, sig)
-    low_perc, hi_perc = norm_dist.cdf(low_rating), norm_dist.cdf(hi_rating)
-    return norm_dist.ppf(np.linspace(low_perc, hi_perc, n))
+    low_perc, high_perc = norm_dist.cdf(low_rating), norm_dist.cdf(hi_rating)
+    return norm_dist.ppf(np.linspace(low_perc, high_perc, n))
 
 
-def generate_logistic_ratings(low_rating, hi_rating, n, mu, sig):
+def generate_logistic_ratings(n: int, low_rating: float=-3., hi_rating: float=3., mu: float=0., sig: float=1.):
+    '''
+    Generate a logistic distribution based on percent range.
+
+    Args:
+        n (:obj:`int`): The number of data points.
+
+        low_rating (:obj:`float`): The lower bound rating.
+
+        hi_rating (:obj:`float`): The upper bound rating.
+
+        mu (:obj:`float`): The expected value (mu) of the normal distribution.
+
+        sig (:obj:`float`): The deviation (sigma) of the normal distribution.
+
+        a (:obj:`float`): Skewness parameter. When a = 0, it is identical to a normal distribution.
+
+    Returns:
+        obj:`np.ndarray`: A logistic distribution of the number of data points.
+    '''
     logistic_dist = logistic(mu, sig)
-    low_perc, hi_perc = logistic_dist.cdf(low_rating), logistic_dist.cdf(hi_rating)
-    return logistic_dist.ppf(np.linspace(low_perc, hi_perc, n))
+    low_perc, high_perc = logistic_dist.cdf(low_rating), logistic_dist.cdf(hi_rating)
+    return logistic_dist.ppf(np.linspace( low_perc, high_perc, n))
 
 
-def generate_skewnormal_ratings(low_rating, hi_rating, n, mu, sig, a):
+def generate_skewnormal_ratings(n: int, low_rating: float=-3., hi_rating: float=3., mu: float=0., sig: float=1., a: float=0.):
+    '''
+    Generate a logistic distribution based on percent range.
+
+    Args:
+        n (:obj:`int`): The number of data points.
+
+        low_rating (:obj:`float`): The lower bound rating.
+
+        hi_ratign (:obj:`float`): The upper bound rating.
+
+        mu (:obj:`float`): The expected value (mu) of the normal distribution.
+
+        sig (:obj:`float`): The deviation (sigma) of the normal distribution.
+
+        a (:obj:`float`): Skewness parameter. When a = 0, it is identical to a normal distribution.
+
+    Returns:
+        obj:`np.ndarray`: A logistic distribution of the number of data points.
+    '''
     norm_dist = skewnorm(loc=mu, scale=sig, a=a)
-    low_perc, hi_perc = norm_dist.cdf(low_rating), norm_dist.cdf(hi_rating)
-    return norm_dist.ppf(np.linspace(low_perc, hi_perc, n))
+    low_perc, high_perc = norm_dist.cdf(low_rating), norm_dist.cdf(hi_rating)
+    return norm_dist.ppf(np.linspace(low_perc, high_perc, n))
 
 
 class MixtureModel(rv_continuous):
